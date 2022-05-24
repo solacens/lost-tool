8ff3e7e4bb9f052e520fb0b46047d5b52ff941c4
solacens <me@solacens.com>
2022-05-25 00:26:44 +0800

    update pyinstaller script

--- Scripts/venv-build.ps1
+++ Scripts/venv-build.ps1
@@ -4,8 +4,6 @@ python -m venv .
 
 . ./Scripts/Activate.ps1
 
-pyinstaller --onefile --icon=img/icon/icon.ico main.pyw
+if (Test-Path -Path dist) { rm -r dist }
 
-if (Test-Path -Path dist/LOST_TOOL.exe) { rm dist/LOST_TOOL.exe }
-
-mv dist/main.exe dist/LOST_TOOL.exe
+pyinstaller --name=LOST_TOOL --icon=img/icon/icon.ico main.pyw
