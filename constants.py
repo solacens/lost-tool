5cd58f4801cb54ae0372a55e6db76bc086c21432
solacens <me@solacens.com>
2022-05-26 13:53:17 +0800

    update constant

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ from pynput.keyboard import Key
 
 LT_SEARCH_WINDOW_TITLE = "LOST ARK"
 LT_REPEATER_KEY_FOR_HOLDING = Key.ctrl_l
-LT_REPEATER_MINIMIUM_TIME = None or 1.0 # Minumium hold for 1s after press
+LT_REPEATER_MINIMIUM_TIME = None or 0.5 # Minumium hold for 1s after press
 LT_PREFERRED_WIDTH = 300
 LT_PREFERRED_HEIGHT = 300
